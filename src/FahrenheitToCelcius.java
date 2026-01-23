02b2f741afa6a35e5681f5e7cb09c0ddb612976e
sayan629 <sayanpal771@gmail.com>
2026-01-24 00:15:49 +0530

    Inputed FahrenheitToCelcius inplemented

--- src/FahrenheitToCelcius.java
+++ src/FahrenheitToCelcius.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class FahrenheitToCelcius {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        double Fahrenheit,celsius;
+        double Fahrenheit,celsius,InputedFahrenheit;
         System.out.print("Enter Temparature (Fahrenheit):");
         Fahrenheit=sc.nextDouble();
 
         celsius = (5.0 / 9.0) * (Fahrenheit - 32);
+        InputedFahrenheit=(9.0/5.0)*celsius+32;
 
+        //celsius = ((int)(celsius * 100)) / 100.0;
+        //InputedFahrenheit = ((int)(InputedFahrenheit * 100)) / 100.0;
+        // truncate to 2 decimal places
 
-        celsius = ((int)(celsius * 100)) / 100.0;  // truncate to 2 decimal places
+        System.out.println("Temparature in Celsius =  "+celsius);
+        System.out.println("Inputed Fahrenheit= "+InputedFahrenheit);
 
-        System.out.print("Temparature in Celsius =  "+celsius);
         sc.close();
 
     }
