6fe84239be2071654810e145f5844b82f2bfc7cd
sayan629 <sayanpal771@gmail.com>
2026-01-31 12:52:09 +0530

    Implement basic calculator using switch-case

---
+++ src/OpCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class OpCalculator {
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Two Numbers: ");
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result;
+        switch (operator){
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator!!");
+                sc.close();
+        }
+    }
+}
