cf3d6123d92bfdcb7b5302c041cd0fdff265747d
sayan629 <sayanpal771@gmail.com>
2026-02-18 00:02:41 +0530

    calculate output using dry run then check

---
+++ src/TrickyOutput.java
@@ -0,0 +1,10 @@
+public class TrickyOutput {
+    static void main(String[] args) {
+        int a=5,b=2;
+        System.out.print(a/b);
+        System.out.print(" "+a%b);
+        System.out.print(" "+a*b);
+        System.out.println(" "+4%2);
+        System.out.println(" "+a+b);
+    }
+}
